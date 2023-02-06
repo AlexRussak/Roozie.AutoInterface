a22fecddcbc501536d0f22f94f35cc353bc2cb8c
Alex Russak <890967+AlexRussak@users.noreply.github.com>
2023-02-05 22:49:59 -0800

    Enable some style rules

--- Roozie.AutoInterface.Tests.Integration/IndexerClass.cs
+++ Roozie.AutoInterface.Tests.Integration/IndexerClass.cs
@@ -3,24 +3,24 @@ namespace Roozie.AutoInterface.Tests.Integration;
 [AutoInterface]
 public class IndexerClass : IIndexerClass
 {
-    private int _value1;
-    private string _value2 = "test";
+    private int value1;
+    private string value2 = "test";
 
-    public int this[int value1]
+    public int this[int val1]
     {
-        get => _value1;
-        set => _value1 = value;
+        get => value1;
+        set => value1 = value;
     }
 
-    public string this[string value2]
+    public string this[string val2]
     {
-        get => _value2;
-        set => _value2 = value;
+        get => value2;
+        set => value2 = value;
     }
 
     public int this[string v1 = "", decimal v2 = 1234.1234m, KnownColor color = KnownColor.Aqua]
     {
-        get => _value1;
-        set => _value1 = value;
+        get => value1;
+        set => value1 = value;
     }
 }
